084ffd4057185fb7633b926c4a646c8abb95cc9f
Mwenda Kanyi <mwenda@kanyilinux.kanyi.bro>
2024-11-29 23:02:19 +0300

    wth schema

--- controllers/tenantIssueController.js
+++ controllers/tenantIssueController.js
@@ -1,14 +1,16 @@
 const Issue = require("../models/Issue");
 const jwt = require("jsonwebtoken");
 const Unit = require("../models/Unit");
+const Tenant = require("../models/Tenant");
 
-exports.getTenantIssues = async (req, res)=>{
-    // get the user id
-    const user = req.user
-    const unit = await Unit.find({tenant:user})
-    const issues = await Issue.find({unit:unit})
+// exports.getTenantIssues = async (req, res)=>{
+//     // get the user id
+//     const user = req.user
+//     const unit = await Unit.find({tenant:user})
+//     const issues = await Issue.find({unit:unit})
 
-    res.status(200).send(issues)
+//     res.status(200).send(issues)
 
 
-}
+// }
+
